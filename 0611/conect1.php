414430fe37dc0e89963d3b992f0f78a56f271566
masu-hi <hikari09.masu@gmail.com>
2026-06-11 12:44:51 +0900

    0611
    データベース

---
+++ 0611/conect1.php
@@ -0,0 +1,21 @@
+<?php
+require_once './functions.php';
+try {
+    //functions.php内の関数を実行
+    $dbh = db_open();
+    //sql文を作った
+    $sql = 'SELECT title, author FROM books';
+    //sql文を実行した->返り値が$statementに入った
+    $statement = $dbh->query($sql);
+
+    //while（ここがtrueのときは）
+    while ($row = $statement->fetch()) {
+        echo "書籍名" . str2html($row[0]) . "<br>";
+        echo "著者名" . str2html($row[1]) . "<br><br>";
+    }
+
+    var_dump($statement);
+} catch (PDOException $e) {
+    echo "エラー！:" . str2html($e->getMessage()) . "<br>";
+    exit;
+}
